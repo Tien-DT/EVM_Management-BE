dc260b67df1100c71f1c00b4a73b37683e092cea
Tien Trong <trongtiensp007@gmail.com>
2025-10-14 10:23:01 +0700

    feat: add api vehicle variant

---
+++ EVMManagement.BLL/DTOs/Response/Vehicle/VehicleVariantResponse.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace EVMManagement.BLL.DTOs.Response.Vehicle
+{
+    public class VehicleVariantResponse
+    {
+        public Guid Id { get; set; }
+        public Guid ModelId { get; set; }
+        public string? Color { get; set; }
+        public int? ChargingTime { get; set; }
+        public string? Engine { get; set; }
+        public decimal? Capacity { get; set; }
+        public string? ShockAbsorbers { get; set; }
+        public string? BatteryType { get; set; }
+        public string? BatteryLife { get; set; }
+        public decimal? MaximumSpeed { get; set; }
+        public string? DistancePerCharge { get; set; }
+        public int? Weight { get; set; }
+        public int? GroundClearance { get; set; }
+        public string? Brakes { get; set; }
+        public int? Length { get; set; }
+        public int? Width { get; set; }
+        public int? Height { get; set; }
+        public decimal? Price { get; set; }
+        public int? TrunkWidth { get; set; }
+        public string? Description { get; set; }
+        public decimal? ChargingCapacity { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
--- EVMManagement.BLL/Services/Interface/IVehicleVariantService.cs
+++ EVMManagement.BLL/Services/Interface/IVehicleVariantService.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using EVMManagement.BLL.DTOs.Request.Vehicle;
+using EVMManagement.BLL.DTOs.Response;
+using EVMManagement.BLL.DTOs.Response.Vehicle;
 using EVMManagement.DAL.Models.Entities;
 
 namespace EVMManagement.BLL.Services.Interface
@@ -7,5 +11,10 @@ namespace EVMManagement.BLL.Services.Interface
     public interface IVehicleVariantService
     {
         Task<VehicleVariant> CreateVehicleVariantAsync(VehicleVariantCreateDto dto);
+        Task<PagedResult<VehicleVariantResponse>> GetAllAsync(int pageNumber = 1, int pageSize = 10);
+        Task<VehicleVariantResponse?> GetByIdAsync(Guid id);
+        Task<VehicleVariantResponse?> UpdateAsync(Guid id, VehicleVariantUpdateDto dto);
+        Task<VehicleVariantResponse?> UpdateIsDeletedAsync(Guid id, bool isDeleted);
+        Task<bool> DeleteAsync(Guid id);
     }
 }
