4da85f975ae33c2a96421b4eaf9e17de5fab03df
Ngan Pham <phamthanhngan04@gmail.com>
2025-10-15 11:53:20 +0700

    fix: refactor code of vehicle model

--- EVMManagement.DAL/Repositories/Interface/IVehicleModelRepository.cs
+++ EVMManagement.DAL/Repositories/Interface/IVehicleModelRepository.cs
@@ -1,13 +1,12 @@
 using EVMManagement.DAL.Models.Entities;
+using EVMManagement.DAL.Models.Enums;
 
 namespace EVMManagement.DAL.Repositories.Interface
 {
     public interface IVehicleModelRepository : IGenericRepository<VehicleModel>
     {
-        Task<IEnumerable<VehicleModel>> GetAllOrderedByCreatedDateDescAsync();
-        Task<IEnumerable<VehicleModel>> GetByRankingAsync(Models.Enums.VehicleModelRanking ranking);
-        Task<VehicleModel?> UpdateAsync(VehicleModel entity);
-        Task<VehicleModel?> UpdateIsDeletedAsync(Guid id, bool isDeleted);
-        Task<IEnumerable<VehicleModel>> SearchByQueryAsync(string q);
+
+        IQueryable<VehicleModel> GetByRankingAsync(VehicleModelRanking ranking);
+        IQueryable<VehicleModel> SearchByQueryAsync(string q);
     }
 }
