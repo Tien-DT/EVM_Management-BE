d854c55f83c98a80e7892b383b145244db3f9c8e
Ngan Pham <phamthanhngan04@gmail.com>
2025-10-25 18:14:21 +0700

    fix: change logic of search available slot for test drive

--- EVMManagement.DAL/Repositories/Interface/IVehicleTimeSlotRepository.cs
+++ EVMManagement.DAL/Repositories/Interface/IVehicleTimeSlotRepository.cs
@@ -6,31 +6,12 @@ using System.Threading.Tasks;
 namespace EVMManagement.DAL.Repositories.Interface
 {
     public interface IVehicleTimeSlotRepository : IGenericRepository<VehicleTimeSlot>
-    {
-        /// <summary>
-        /// Get all vehicles of a variant at a dealer
-        /// </summary>
-        Task<List<(Guid Id, string Vin)>> GetAvailableVehiclesByVariantAndDealerAsync(Guid variantId, Guid dealerId);
-
-        /// <summary>
-        /// Get booked/available VehicleTimeSlots for a variant in date range
-        /// </summary>
-        Task<List<VehicleTimeSlot>> GetSlotsByVariantInDateRangeAsync(Guid modelId, Guid variantId, Guid dealerId, DateTime? fromDate, DateTime? toDate);
-
-        /// <summary>
-        /// Get available VehicleTimeSlots for a specific vehicle in date range
-        /// </summary>
-        Task<List<VehicleTimeSlot>> GetAvailableSlotsByVehicleInDateRangeAsync(Guid vehicleId, DateTime fromDate, DateTime toDate);
-
-        /// <summary>
-        /// Get available VehicleTimeSlots by date (for scheduling screen)
-        /// </summary>
-        Task<List<VehicleTimeSlot>> GetAvailableSlotsByDateAsync(Guid dealerId, DateTime slotDate);
-
-        /// <summary>
-        /// Get VehicleTimeSlots for a specific slot and date with full details
-        /// </summary>
-        Task<List<VehicleTimeSlot>> GetSlotsByDateAndMasterSlotAsync(Guid modelId, Guid variantId, Guid dealerId, DateTime slotDate, Guid masterSlotId);
+    {       
+        Task<Dictionary<DateTime, List<(Guid MasterSlotId, int StartOffsetMinutes, int DurationMinutes, int AvailableCount)>>> 
+            GetSlotSummariesByModelAsync(Guid modelId, Guid dealerId, DateTime? fromDate, DateTime? toDate);
+        
+        Task<List<(Guid VehicleId, string Vin)>> 
+            GetAvailableVehiclesBySlotAsync(Guid modelId, Guid dealerId, DateTime slotDate, Guid masterSlotId);
     }
 }
 
