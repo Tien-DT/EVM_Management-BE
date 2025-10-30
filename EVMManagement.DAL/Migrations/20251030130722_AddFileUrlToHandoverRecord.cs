c064354a50d8e0fd804635c16073578373aa659e
Tien Trong <trongtiensp007@gmail.com>
2025-10-30 20:08:04 +0700

    update: add migration

---
+++ EVMManagement.DAL/Migrations/20251030130722_AddFileUrlToHandoverRecord.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EVMManagement.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFileUrlToHandoverRecord : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FileUrl",
+                table: "HandoverRecords",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileUrl",
+                table: "HandoverRecords");
+        }
+    }
+}
