9c844cfe34c7a8652edf0c07fdea2686066734f5
Ngan Pham <phamthanhngan04@gmail.com>
2025-11-12 23:20:23 +0700

    update: using service createCustomerAsync instead unitOfWork

--- EVMManagement.BLL/Services/Class/TestDriveBookingService.cs
+++ EVMManagement.BLL/Services/Class/TestDriveBookingService.cs
@@ -136,10 +136,12 @@ namespace EVMManagement.BLL.Services.Class
 
                     _unitOfWork.Customers.Update(existingCustomer);
                     customer = existingCustomer;
+                    await _unitOfWork.SaveChangesAsync();
                 }
                 else
                 {
-                    customer = new Customer
+                    
+                    var customerCreateDto = new CustomerCreateDto
                     {
                         Phone = dto.CustomerPhone,
                         FullName = dto.CustomerFullName,
@@ -150,10 +152,9 @@ namespace EVMManagement.BLL.Services.Class
                         CardId = dto.CustomerCardId,
                         DealerId = dto.DealerId
                     };
-                    await _unitOfWork.Customers.AddAsync(customer);
-                }
 
-                await _unitOfWork.SaveChangesAsync();
+                    customer = await _customerService.CreateCustomerAsync(customerCreateDto);
+                }
 
                 var existingVehicleTimeSlot = await _unitOfWork.VehicleTimeSlots.GetQueryable()
                     .FirstOrDefaultAsync(vts => 
